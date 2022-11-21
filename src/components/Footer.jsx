f781ac2dffde09a238ebc62da40a163a2311b906
educaba123 <u202019644@upc.edu.pe>
2022-11-20 20:11:33 -0500

    rama-tb4

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Footer() {
+    return(
+        <nav>
+            <ul className="inferior">
+                <li>
+                    <a href="#" className="active">
+                        Dieta
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Horario
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Premium
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Footer;
